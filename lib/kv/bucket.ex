b34042de61eef5a2920e8668332709ebf57a4c02
Mateus Luiz <mateuslfreitasb@gmail.com>
2019-02-05 16:46:33 -0200

    Add docs

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -1,18 +1,30 @@
 defmodule KV.Bucket do
   use Agent
 
+  @doc """
+  Starts the bucket
+  """
   def start_link do
     Agent.start_link fn -> %{} end
   end
 
+  @doc """
+  Gets one value from the bucket given a key
+  """
   def get(bucket, key) do
     Agent.get bucket, &Map.get(&1, key)
   end
 
+  @doc """
+  Adds or updated one value from the bucket given a key and a value
+  """
   def put(bucket, key, value) do
     Agent.update bucket, &Map.put(&1, key, value)
   end
 
+  @doc """
+  Removes one key and value pair from the given bucket
+  """
   def delete(bucket, key) do
     Agent.get_and_update bucket, &Map.pop(&1, key)
   end
