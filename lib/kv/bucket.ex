0f06db4278b80a0636f50683cb089f24eec1b7ba
Mateus Luiz <mateuslfreitasb@gmail.com>
2019-02-05 16:44:28 -0200

    Implement the delete function

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -2,14 +2,18 @@ defmodule KV.Bucket do
   use Agent
 
   def start_link do
-    Agent.start_link(fn -> %{} end)
+    Agent.start_link fn -> %{} end
   end
 
   def get(bucket, key) do
-    Agent.get(bucket, &Map.get(&1, key))
+    Agent.get bucket, &Map.get(&1, key)
   end
 
   def put(bucket, key, value) do
-    Agent.update(bucket, &Map.put(&1, key, value))
+    Agent.update bucket, &Map.put(&1, key, value)
+  end
+
+  def delete(bucket, key) do
+    Agent.get_and_update bucket, &Map.pop(&1, key)
   end
 end
--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -1,14 +1,23 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "returns nil for non matching key" do
+  setup do
     {:ok, bucket} = KV.Bucket.start_link()
+    %{bucket: bucket}
+  end
+
+  test "returns nil for non matching key", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "milk") == nil
   end
 
-  test "returns the stored key value" do
-    {:ok, bucket} = KV.Bucket.start_link()
+  test "returns the stored key value", %{bucket: bucket} do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "removes the stores key value", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "milk", 3)
+    KV.Bucket.delete(bucket, "milk")
+    assert KV.Bucket.get(bucket, "milk") == nil
   end
 end
