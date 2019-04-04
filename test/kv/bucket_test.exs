8dcfd4e554e7cfd12b825843a271ae70bcefbe1b
Mateus Luiz <mateuslfreitasb@gmail.com>
2019-04-04 20:13:25 -0300

    Add test to the delete function

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -15,7 +15,12 @@ defmodule KV.BucketTest do
     assert KV.Bucket.get(bucket, "milk") == 3
   end
 
-  test "removes the stores key value", %{bucket: bucket} do
+  test "removes returns the deleted key's value", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.delete(bucket, "milk") == 3
+  end
+
+  test "removes the stored tuple", %{bucket: bucket} do
     KV.Bucket.put(bucket, "milk", 3)
     KV.Bucket.delete(bucket, "milk")
     assert KV.Bucket.get(bucket, "milk") == nil
