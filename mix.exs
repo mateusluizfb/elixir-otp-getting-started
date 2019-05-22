9355fea1a8d045cbf27f586ea8c117c3a1cc6d2e
Mateus Luiz <mateuslfreitasb@gmail.com>
2019-05-22 19:52:54 -0300

    Add Mock lib

--- mix.exs
+++ mix.exs
@@ -11,17 +11,29 @@ defmodule KV.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  def set_application(:test) do
+    [
+      extra_applications: [:logger],
+    ]
+  end
+
+  def set_application(_) do
     [
       extra_applications: [:logger],
       mod: {KV, []}
     ]
   end
 
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    set_application(Mix.env())
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:mock, "~> 0.3.0", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
