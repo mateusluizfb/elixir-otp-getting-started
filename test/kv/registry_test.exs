18ffd32834f1bbdee5da852a1c826c3063b551b1
Mateus Luiz <mateuslfreitasb@gmail.com>
2019-05-22 20:05:35 -0300

    Add mock test

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -5,6 +5,8 @@
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
+  import Mock
+
   setup do
     registry = start_supervised!(KV.RegistryClient) # Impede que os testes interfiram entre si
     %{registry: registry}
@@ -32,12 +34,25 @@ defmodule KV.RegistryTest do
     assert KV.RegistryClient.lookup(registry, "GenServer bucket") == :error
   end
 
+  @tag :skip
   test "Supervisor will revive the dead gensever" do
     {:ok, pid} = KV.Supervisor.start_link([])
     KV.RegistryClient.create KV.RegistryClient, "wallet"
     [h | _] = Supervisor.which_children(pid)
     {KV.RegistryClient, pid_reg, _, _} = h
     send(pid_reg, :insta_kill)
+
     assert %{active: 1} = Supervisor.count_children(pid)
+  end
+
+  test_with_mock "calls the Bucket 'start_link'",  %{ registry: registry }, KV.Bucket, [],
+    [
+      start_link: fn ->
+        Agent.start_link fn -> %{} end
+      end
+    ] do
+      
+    KV.RegistryClient.create(registry, "GenServer bucket")
+    assert_called KV.Bucket.start_link
   end
 end
